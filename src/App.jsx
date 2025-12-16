f81cb213e2cac576666fad84d327e247b3d6e3c0
aradhya-vermaa <verma48@wisc.edu>
2025-12-15 22:32:04 -0600

    Import TechnicalMethods component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import TechnicalMethods from "./components/TechnicalMethods";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Motivation from "./components/Motivation";
